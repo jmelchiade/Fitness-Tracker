0ebe38baf902196a3155aebd5682a2cdacbd3a41
Jenniffer Melchiade <jdenisemelch@gmail.com>
2022-11-08 18:42:45 -0500

    reviewed tests for routine activities

--- db/routine_activities.js
+++ db/routine_activities.js
@@ -1,28 +1,21 @@
-const client = require('./client')
+const client = require("./client");
 
-async function getRoutineActivityById(id){
-}
+async function getRoutineActivityById(id) {}
 
 async function addActivityToRoutine({
   routineId,
   activityId,
   count,
   duration,
-}) {
-    
-}
+}) {}
 
-async function getRoutineActivitiesByRoutine({id}) {
-}
+async function getRoutineActivitiesByRoutine({ id }) {}
 
-async function updateRoutineActivity ({id, ...fields}) {
-}
+async function updateRoutineActivity({ id, ...fields }) {}
 
-async function destroyRoutineActivity(id) {
-}
+async function destroyRoutineActivity(id) {}
 
-async function canEditRoutineActivity(routineActivityId, userId) {
-}
+async function canEditRoutineActivity(routineActivityId, userId) {}
 
 module.exports = {
   getRoutineActivityById,
